8a56d78e28de6e270d04b10fbc05b0998ef55284
Sean McQueen <sean@mcqueen.net>
2013-09-23 10:20:57 -0700

    date on the left panel, more UI sweetness

--- routes/index.js
+++ routes/index.js
@@ -34,14 +34,7 @@ exports.index = function(req, res){
       var date = new Date();
       var dd = date.getDate();
       var mm = date.getMonth()+1; //January is 0!
-      var yyyy = date.getFullYear();
-      if(dd < 10) {
-          dd='0'+dd
-      } 
-      if(mm < 10) {
-          mm='0'+mm
-      }
-      var title = mm + '/' + dd + '/' +yyyy;
+      var title = mm + '/' + dd;
 
       res.render('index',
         {
