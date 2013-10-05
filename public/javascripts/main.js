c49a6d3a78fe4570ec31042b10d4308c92a69a34
Remy Guercio <remyguercio@yahoo.com>
2013-10-04 20:58:30 -0700

    Initial jquery main file. Loads api, but is not dynamic yet.

---
+++ public/javascripts/main.js
@@ -0,0 +1,66 @@
+var dataSet = {};
+var days = {
+  0: "sun",
+  1: "mon",
+  2: "tue",
+  3: "wed",
+  4: "thu",
+  5: "fri",
+  6: "sat"
+}
+// assumes it passes in breakfast, lunch, dinner, brunch (on correct days)
+function update (current_day, current_time) {
+  var today = days[current_day];
+  var items = [];
+
+  $('div#collins').html('');
+  $('div#scripps').html('');
+  $('div#mudd').html('');
+  $('div#pitzer').html('');
+  $('div#frary').html('');
+  $('div#frank').html('');
+
+
+  $.each(dataSet.collins.today.current_time, function(i, item) {
+    items.push('<li>'.item.'</li>');
+  });
+  $('div#collins').append( items.join('') );
+  items = [];
+  $.each(dataSet.scripps.today.current_time, function(i, item) {
+    items.push('<li>'.item.'</li>');
+  });
+  $('div#scripps').append( items.join('') );
+  items = [];
+  $.each(dataSet.mudd.today.current_time, function(i, item) {
+    items.push('<li>'.item.'</li>');
+  });
+  $('div#mudd').append( items.join('') );
+  items = [];
+  $.each(dataSet.pitzer.today.current_time, function(i, item) {
+    items.push('<li>'.item.'</li>');
+  });
+  $('div#pitzer').append( items.join('') );
+  items = [];
+  $.each(dataSet.frary.today.current_time, function(i, item) {
+    items.push('<li>'.item.'</li>');
+  });
+  $('div#frary').append( items.join('') );
+  items = [];
+  $.each(dataSet.frank.today.current_time, function(i, item) {
+    items.push('<li>'.item.'</li>');
+  });
+  $('div#frank').append( items.join('') );
+
+
+}
+$(document).ready(function() {
+  $.ajax({
+      url: "http://dining-api.herokuapp.com/all",
+      success: function(data) {
+        if (data) {
+          dataSet = data;
+          update(5,"dinner");
+        }
+      }
+  });
+});
